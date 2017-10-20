2b6d4ce119797e095c0f593140fe6b0a339e4459
Joe Ferraro <ferraro.joseph@gmail.com>
2017-10-19 20:05:58 -0400

    support RN 0.40+

--- RNRandomBytes.h
+++ RNRandomBytes.h
@@ -7,8 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
+
+#if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
+#else
+#import <React/RCTBridgeModule.h>
+#endif
+
+#if __has_include("RCTBridge.h")
 #import "RCTBridge.h"
+#else
+#import <React/RCTBridge.h>
+#endif
 
 @interface RNRandomBytes : NSObject<RCTBridgeModule>
 
--- RNRandomBytes.m
+++ RNRandomBytes.m
@@ -5,10 +5,19 @@
 //  Created by Mark Vayngrib on 10/13/15.
 //  Copyright (c) 2015 Facebook. All rights reserved.
 //
-
 #import "RNRandomBytes.h"
+
+#if __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
+#else
+#import <React/RCTBridgeModule.h>
+#endif
+
+#if __has_include("RCTBridge.h")
 #import "RCTBridge.h"
+#else
+#import <React/RCTBridge.h>
+#endif
 
 @implementation RNRandomBytes
 
